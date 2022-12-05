7547429108bef1f5a26ed39aeb9300461ed599d6
Engineer <arsenya.suvorov@mail.ru>
2022-12-05 22:40:02 +0400

    start

--- Task41/Program.cs
+++ Task41/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Console.Write("Введите числа (через пробел): ");
+// int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+// int count = 0;
+ 
+// for (int i = 0; i < arr.Length; i++)
+// {
+//     if (arr[i] > 0)
+//     {
+//         count++;
+//     }
+// }
+ 
+// Console.WriteLine($"Кол-во элементов больше нуля: {count}");
+
+Console.WriteLine("Введите количество чисел, которые хотите ввести:");
+            int len =  int.Parse(Console.ReadLine());
+            int[] arr = new int[len];
+            Console.WriteLine("Введите числа через (Пробел):");
+            string[] arrayNumber =  Console.ReadLine().Split(' ');
+ 
+            for (int i =0; i < arrayNumber.Length; i++)
+            {
+                arr[i] = int.Parse(arrayNumber[i]);
+            }
+int count = 0;
+ 
+for (int i = 0; i < len; i++)
+{
+    if (arr[i] > 0)
+    {
+        count++;
+    }
+} 
+Console.WriteLine($"Кол-во элементов больше нуля: {count}");
