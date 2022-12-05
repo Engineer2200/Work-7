430fb99282b3f3fd23ccb7fe3072509284cceeeb
Engineer <arsenya.suvorov@mail.ru>
2022-12-05 23:08:26 +0400

    start

--- Task43/Program.cs
+++ Task43/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("Введите значение b1: ");
+var b1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите значение b2: ");
+var b2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите значение k1: ");
+var k1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите значение k2: ");
+var k2 = Convert.ToDouble(Console.ReadLine());
+
+var x = -(b1 - b2) / (k1 - k2);
+var y = k1 * x + b1;
+
+Console.WriteLine($"Пересечение в точке: ({x};{y})");
